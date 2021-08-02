7c23280632e51d20ebe301b4fdfa00920ec47fd2
Алексей Чистов <aleksey.chistov@gmail.com>
2021-08-02 09:52:04 +0300

    .zshrc.local.symlink

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,1 +1,7 @@
+echo "Установка ~/.zshrc"
 ln -s -f $(pwd)/zsh/.zshrc.symlink ~/.zshrc
+
+if [ -f "$(pwd)/zsh/.zshrc.local.symlink" ]; then
+    echo "Найдена локальная конфигурация ~/.zshrc.local"
+    ln -s -f $(pwd)/zsh/.zshrc.local.symlink ~/.zshrc.local
+fi
