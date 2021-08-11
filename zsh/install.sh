55aaf92e067009695b7903f79a674a190656052d
Hulvdan <aleksey.chistov@gmail.com>
2021-08-11 18:04:46 +0300

    Better paths; check zsh existence; installing oh-my-zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,3 +1,13 @@
+if ! [ -x "$(command -v zsh)" ]; then
+  echo 'Error: ZSH не установлен!' >&2
+  exit 1
+fi
+
+if [ ! -f "$HOME/.oh-my-zsh/oh-my-zsh.sh" ]; then
+    echo "Скачивание и установка oh-my-zsh"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
 echo "Установка ~/.zshrc"
 ln -s -f $(pwd)/zsh/.zshrc.symlink ~/.zshrc
 
