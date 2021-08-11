9ff86422beb9ad7ed914b4692e0df4210b55da3a
Алексей Чистов <aleksey.chistov@gmail.com>
2021-08-11 09:58:45 +0300

    Своя тема, сделанная из Agnoster

--- zsh/install.sh
+++ zsh/install.sh
@@ -5,3 +5,6 @@ if [ -f "$(pwd)/zsh/.zshrc.local.symlink" ]; then
     echo "Найдена локальная конфигурация ~/.zshrc.local"
     ln -s -f $(pwd)/zsh/.zshrc.local.symlink ~/.zshrc.local
 fi
+
+echo "Установка темы hulvdan"
+ln -s -f $(pwd)/zsh/themes/hulvdan.zsh-theme.symlink ~/.oh-my-zsh/themes/hulvdan.zsh-theme
