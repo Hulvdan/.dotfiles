33628cc65a413a753de3d4aa5138092d9b232783
Алексей Чистов <aleksey.chistov@gmail.com>
2021-08-12 00:26:25 +0300

    Более подробное описание + проверки на установку vim/tmux

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,1 +1,6 @@
+if ! [ -x "$(command -v tmux)" ]; then
+  echo 'Error: TMUX не установлен!' >&2
+  exit 1
+fi
+
 ln -s -f $(pwd)/tmux/.tmux.conf.symlink ~/.tmux.conf
--- vim/install.sh
+++ vim/install.sh
@@ -1,1 +1,6 @@
+if ! [ -x "$(command -v vim)" ]; then
+  echo 'Error: VIM не установлен!' >&2
+  exit 1
+fi
+
 ln -s -f $(pwd)/vim/.vimrc.symlink ~/.vimrc
