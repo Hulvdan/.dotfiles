110aaa29d1cabd621259b4e47f7a1e465a16ffe6
Aleksey Chistov <aleksey.chistov@gmail.com>
2023-02-22 01:29:51 +0600

    Removed vim configuration

--- vim/install.sh
+++
@@ -1,8 +0,0 @@
-if ! [ -x "$(command -v vim)" ]; then
-  echo 'Error: VIM не установлен!' >&2
-  exit 1
-fi
-
-ln -s -f $(pwd)/vim/.vimrc.symlink ~/.vimrc
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
-
