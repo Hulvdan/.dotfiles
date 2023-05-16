4b15a7692e91735ac6b8127d2540f1a630398c79
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-05-17 01:03:37 +0600

    :dizzy: (darwin) Now it is possible to select specific wallpaper

--- darwin/change_wallpaper.sh
+++ darwin/change_wallpaper.sh
@@ -1,7 +1,15 @@
-wallpaper_name=`ls -1 /Users/user/Pictures/Wallpapers/Collection | python -c "import sys; import random; print(random.choice(sys.stdin.readlines()).rstrip())"`
+wallpaper_name=''
+if [ "$1" = '' ]; then
+    wallpaper_name=`ls -1 /Users/user/Pictures/Wallpapers/Collection | python -c "import sys; import random; print(random.choice(sys.stdin.readlines()).rstrip())"`
+    echo Random wallpaper of the day: $wallpaper_name
+else
+    wallpaper_name=$(ls -1 $1 | sed 's|/Users/user/Pictures/Wallpapers/Collection/||')
+fi
+
 string=$(cat << EOF
 repeat 9 times
     tell application "System Events" to tell every desktop to set picture to "/Users/user/Pictures/Wallpapers/Collection/$wallpaper_name"
+    delay 0.5
     tell application "System Events" to keystroke (ASCII character 29) using {control down}
     delay 1
 end
