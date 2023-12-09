488aec8f14fb2c04883533fd897b9a8d3d3ef72e
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-12-10 03:19:39 +0700

    Update set.lua

--- nvim/lua/hulvdan/set.lua
+++ nvim/lua/hulvdan/set.lua
@@ -1,30 +1,42 @@
+-- Essential shiet
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.errorbells = false
+vim.opt.belloff = "all"
+vim.opt.termguicolors = true
+vim.opt.hidden = true
 
+-- Tabs and indent settings
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
-vim.opt.wrap = true
-
+-- Swap files
 vim.opt.swapfile = false
 vim.opt.backup = false
+
+-- Undo files
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir/"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+-- Number of lines to keep above/below the cursor
+vim.opt.scrolloff = 8
+
+-- Column with extra info
+vim.opt.signcolumn = "yes"
+vim.opt.foldcolumn = "1"
+
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "100"
 
+vim.opt.wrap = true
