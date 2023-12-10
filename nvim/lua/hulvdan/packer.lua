26145c3d7e697d643112099b30ccd661984a8b2a
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-12-10 13:00:38 +0700

    nvim: Removed vim-fugitive

--- nvim/lua/hulvdan/packer.lua
+++ nvim/lua/hulvdan/packer.lua
@@ -17,7 +17,6 @@ return require('packer').startup(function(use)
 
   use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   use('mbbill/undotree')
-  use('tpope/vim-fugitive')
 
   use {
     'VonHeikemen/lsp-zero.nvim',
