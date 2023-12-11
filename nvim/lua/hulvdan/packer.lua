524613e6b5df25458a858b4eac0a0acd8dbc1800
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-12-11 12:05:50 +0700

    nvim: Configured barbar

--- nvim/lua/hulvdan/packer.lua
+++ nvim/lua/hulvdan/packer.lua
@@ -10,7 +10,7 @@ return require('packer').startup(function(use)
     requires = { { 'nvim-lua/plenary.nvim' } }
   }
 
-  use { "ellisonleao/gruvbox.nvim" }
+  use { 'ellisonleao/gruvbox.nvim' }
 
   -- TODO: CHECK ON OTHER DEVICES!
   require 'nvim-treesitter.install'.prefer_git = false
@@ -28,8 +28,58 @@ return require('packer').startup(function(use)
     },
   })
 
-
   use('romgrk/barbar.nvim')
+  require('barbar').setup({
+    animation = false,
+    auto_hide = 1,
+    icons = {
+      -- Configure the base icons on the bufferline.
+      -- Valid options to display the buffer index and -number are `true`, 'superscript' and 'subscript'
+      buffer_index = false,
+      buffer_number = false,
+      button = '',
+      -- Enables / disables diagnostic symbols
+      diagnostics = {
+        [vim.diagnostic.severity.ERROR] = {enabled = true, icon = 'E'},
+        [vim.diagnostic.severity.WARN] = {enabled = false},
+        [vim.diagnostic.severity.INFO] = {enabled = false},
+        [vim.diagnostic.severity.HINT] = {enabled = false},
+      },
+      gitsigns = {
+        added = {enabled = false, icon = '+'},
+        changed = {enabled = false, icon = '~'},
+        deleted = {enabled = false, icon = '-'},
+      },
+      filetype = {
+        -- Sets the icon's highlight group.
+        -- If false, will use nvim-web-devicons colors
+        custom_colors = false,
+
+        -- Requires `nvim-web-devicons` if `true`
+        enabled = false,
+      },
+      separator = {left = '▎', right = ''},
+
+      -- If true, add an additional separator at the end of the buffer list
+      separator_at_end = true,
+
+      -- Configure the icons on the bufferline when modified or pinned.
+      -- Supports all the base icon options.
+      modified = {button = ''},
+      pinned = {button = '●', filename = true},
+
+      -- Use a preconfigured buffer appearance— can be 'default', 'powerline', or 'slanted'
+      preset = 'default',
+
+      -- Configure the icons on the bufferline based on the visibility of a buffer.
+      -- Supports all the base icon options, plus `modified` and `pinned`.
+      alternate = {filetype = {enabled = false}},
+      current = {buffer_index = false},
+      inactive = {button = ''},
+      visible = {modified = {buffer_number = false}},
+    },
+  })
+
   use('gpanders/editorconfig.nvim')
 
   use {
