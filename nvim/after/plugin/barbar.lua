2b22aa463bf788fbd40a4a91e597a58e4568e267
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-12-11 11:29:05 +0700

    nvim: Moved barbar config

--- nvim/after/plugin/barbar.lua
+++ nvim/after/plugin/barbar.lua
@@ -10,7 +10,8 @@ function tablelength(T)
   return count
 end
 
-vim.keymap.set("n", "C-U", "BufferRestore<CR>")
+vim.keymap.set("n", "C-S-T", ":BufferRestore<CR>")
+vim.keymap.set("n", "<leader>q", ":BufferClose<CR>")
 
 vim.keymap.set("n", "<leader>v", function()
   local current = vim.api.nvim_get_current_buf()
@@ -24,12 +25,12 @@ vim.keymap.set("n", "<leader>v", function()
 
   for i, buffer_number in ipairs(state.buffers) do
     if buffer_number == current then
-      if i == 1 then
-        i = count + 1
+      i = i - 1
+      if i <= 0 then
+        i = count
       end
 
-      vim.cmd(string.format("BufferGoto %s", i - 1), false)
-
+      vim.cmd(string.format("BufferGoto %s", i), false)
       break
     end
   end
@@ -47,14 +48,65 @@ vim.keymap.set("n", "<leader>m", function()
 
   for i, buffer_number in ipairs(state.buffers) do
     if buffer_number == current then
-      if (i == count) then
-        i = 0
+      i = i + 1
+      if (i > count) then
+        i = 1
       end
 
-      vim.cmd(string.format("BufferGoto %s", i + 1), false)
+      vim.cmd(string.format("BufferGoto %s", i), false)
     end
   end
 end)
 
-vim.keymap.set("n", "<leader>q", ":BufferClose<CR>")
+
+require('barbar').setup({
+  animation = false,
+  auto_hide = 1,
+  icons = {
+    -- Configure the base icons on the bufferline.
+    -- Valid options to display the buffer index and -number are `true`, 'superscript' and 'subscript'
+    buffer_index = false,
+    buffer_number = false,
+    button = '',
+    -- Enables / disables diagnostic symbols
+    diagnostics = {
+      [vim.diagnostic.severity.ERROR] = {enabled = true, icon = 'ﬀ'},
+      [vim.diagnostic.severity.WARN] = {enabled = false},
+      [vim.diagnostic.severity.INFO] = {enabled = false},
+      [vim.diagnostic.severity.HINT] = {enabled = true},
+    },
+    gitsigns = {
+      added = {enabled = true, icon = '+'},
+      changed = {enabled = true, icon = '~'},
+      deleted = {enabled = true, icon = '-'},
+    },
+    filetype = {
+      -- Sets the icon's highlight group.
+      -- If false, will use nvim-web-devicons colors
+      custom_colors = false,
+
+      -- Requires `nvim-web-devicons` if `true`
+      enabled = false,
+    },
+    separator = {left = '▎', right = ''},
+
+    -- If true, add an additional separator at the end of the buffer list
+    separator_at_end = true,
+
+    -- Configure the icons on the bufferline when modified or pinned.
+    -- Supports all the base icon options.
+    modified = {button = '●'},
+    pinned = {button = '', filename = true},
+
+    -- Use a preconfigured buffer appearance— can be 'default', 'powerline', or 'slanted'
+    preset = 'default',
+
+    -- Configure the icons on the bufferline based on the visibility of a buffer.
+    -- Supports all the base icon options, plus `modified` and `pinned`.
+    alternate = {filetype = {enabled = false}},
+    current = {buffer_index = false},
+    inactive = {button = '×'},
+    visible = {modified = {buffer_number = false}},
+  },
+})
 
