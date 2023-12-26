4120836976f12c6251eaf0e3d867ebae5199bf51
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-12-26 16:01:21 +0700

    nvim: bound rename + goto messages

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -3,7 +3,11 @@ local lsp = require("lsp-zero")
 lsp.preset('recommended')
 lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
-  local opts = {buffer = bufnr}
+  local opts = {buffer = bufnr, remap = false, silent = true}
+
+  vim.keymap.set('n', '<f2>', vim.diagnostic.goto_next, opts)
+  vim.keymap.set('n', '<S-f2>', vim.diagnostic.goto_prev, opts)
+  vim.keymap.set('n', '<S-f6>', vim.lsp.buf.rename, opts)
 
   vim.keymap.set('n', 'gd', '<CMD>Telescope lsp_definitions<CR>', opts)
   vim.keymap.set('n', 'gi', '<CMD>Telescope lsp_implementations<CR>', opts)
