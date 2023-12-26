90dab9080ac4a918bd95e01fbd0280e65586e5ab
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-12-27 02:14:03 +0700

    nvim: better colored EOL

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,10 +1,11 @@
 function SetTheme(theme)
-	theme = theme or "gruvbox"
-	vim.cmd.colorscheme(theme)
+    theme = theme or "gruvbox"
+    vim.cmd.colorscheme(theme)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 SetTheme()
+vim.fn.execute("hi NonText guifg=black")
 
