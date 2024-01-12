5600797a5c7714651bbf4e974ebae3c8d2e8b398
Aleksei Chistov <aleksey.chistov@gmail.com>
2024-01-13 03:57:45 +0700

    nvim: :H cht.sh

---
+++ nvim/lua/hulvdan/chtsh.lua
@@ -0,0 +1,10 @@
+function chtsh(opts)
+    run = opts.args or nil
+    local query = opts.args
+
+    local url = [[https://cht.sh/]] .. query:gsub("% ", "+")
+    print("Checking url: " .. url)
+    vim.fn.execute([[term curl ]] .. url)
+end
+
+vim.api.nvim_create_user_command('H', chtsh, { nargs='?' })
--- nvim/lua/hulvdan/init.lua
+++ nvim/lua/hulvdan/init.lua
@@ -1,3 +1,4 @@
 require("hulvdan/set")
 require("hulvdan/packer")
 require("hulvdan/remap")
+require("hulvdan/chtsh")
