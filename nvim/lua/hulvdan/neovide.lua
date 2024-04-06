80fc91c699dbd679f44b0bc3499e968c887946d1
Aleksei Chistov <aleksey.chistov@gmail.com>
2024-04-06 16:55:48 +0700

    Update neovide.lua

--- nvim/lua/hulvdan/neovide.lua
+++ nvim/lua/hulvdan/neovide.lua
@@ -1,10 +1,19 @@
-vim.g.hulvdan_fontsize = 13
+vim.g.hulvdan_fontsize = 14
 vim.g.neovide_scroll_animation_length = 0.16
 vim.g.neovide_hide_mouse_when_typing = true
 
+local high_cap = 22;
+local low_cap = 10;
 
 function AdjustFontSize(amount)
     vim.g.hulvdan_fontsize = vim.g.hulvdan_fontsize + amount
+    if vim.g.hulvdan_fontsize > high_cap then
+        vim.g.hulvdan_fontsize = high_cap
+    end
+    if vim.g.hulvdan_fontsize < low_cap then
+        vim.g.hulvdan_fontsize = low_cap
+    end
+
     local f = "For use only at ilovetypography.com"
     -- local f = "For use at ilovetypography.com only"
     -- local f = "Comic Mono"
@@ -20,4 +29,4 @@ vim.keymap.set("n", "<C-_>", function() AdjustFontSize(-font_step) end, opts)
 vim.keymap.set("n", "<C-+>", function() AdjustFontSize(font_step) end, opts)
 
 AdjustFontSize(0)
-vim.fn.execute("set linespace=-1")
+-- vim.fn.execute("set linespace=-1")
