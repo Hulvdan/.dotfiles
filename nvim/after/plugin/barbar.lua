42a39324bbea592f227f4f5e464e76be024d10cb
Aleksei Chistov <aleksey.chistov@gmail.com>
2024-04-23 22:57:05 +0700

    nvim: replaced barbar with bufferline

--- nvim/after/plugin/barbar.lua
+++ nvim/after/plugin/barbar.lua
@@ -1,75 +1,75 @@
-function tablelength(T)
-    local count = 0
-    for _ in pairs(T) do
-        count = count + 1
-    end
+-- function tablelength(T)
+--     local count = 0
+--     for _ in pairs(T) do
+--         count = count + 1
+--     end
 
-    return count
-end
+--     return count
+-- end
 
-function select_previous_tab()
-    vim.g.hulvdan_DisableAnimations()
+-- function select_previous_tab()
+--     vim.g.hulvdan_DisableAnimations()
 
-    local current = vim.api.nvim_get_current_buf()
-    local state = require('barbar.state')
-    local count = tablelength(state.buffers)
+--     local current = vim.api.nvim_get_current_buf()
+--     local state = require('barbar.state')
+--     local count = tablelength(state.buffers)
 
-    if current == nil then
-        vim.cmd(string.format("BufferGoto %s", count), false)
-        vim.fn.execute("NvuiAnimationsEnabled v:true")
-        return
-    end
+--     if current == nil then
+--         vim.cmd(string.format("BufferGoto %s", count), false)
+--         vim.fn.execute("NvuiAnimationsEnabled v:true")
+--         return
+--     end
 
-    for i, buffer_number in ipairs(state.buffers) do
-        if buffer_number == current then
-            i = i - 1
-            if i <= 0 then
-                i = count
-            end
+--     for i, buffer_number in ipairs(state.buffers) do
+--         if buffer_number == current then
+--             i = i - 1
+--             if i <= 0 then
+--                 i = count
+--             end
 
-            vim.cmd(string.format("BufferGoto %s", i), false)
-            break
-        end
-    end
+--             vim.cmd(string.format("BufferGoto %s", i), false)
+--             break
+--         end
+--     end
 
-    vim.g.hulvdan_EnableAnimations()
-end
+--     vim.g.hulvdan_EnableAnimations()
+-- end
 
-function select_next_tab()
-    vim.g.hulvdan_DisableAnimations()
+-- function select_next_tab()
+--     vim.g.hulvdan_DisableAnimations()
 
-    local current = vim.api.nvim_get_current_buf()
-    local state = require('barbar.state')
-    local count = tablelength(state.buffers)
+--     local current = vim.api.nvim_get_current_buf()
+--     local state = require('barbar.state')
+--     local count = tablelength(state.buffers)
 
-    if current == nil then
-        vim.cmd(string.format("BufferGoto %s", count), false)
-        vim.fn.execute("NvuiAnimationsEnabled v:true")
-        return
-    end
+--     if current == nil then
+--         vim.cmd(string.format("BufferGoto %s", count), false)
+--         vim.fn.execute("NvuiAnimationsEnabled v:true")
+--         return
+--     end
 
-    for i, buffer_number in ipairs(state.buffers) do
-        if buffer_number == current then
-            i = i + 1
-            if (i > count) then
-                i = 1
-            end
+--     for i, buffer_number in ipairs(state.buffers) do
+--         if buffer_number == current then
+--             i = i + 1
+--             if (i > count) then
+--                 i = 1
+--             end
 
-            vim.cmd(string.format("BufferGoto %s", i), false)
-        end
-    end
+--             vim.cmd(string.format("BufferGoto %s", i), false)
+--         end
+--     end
 
-    vim.g.hulvdan_EnableAnimations()
-end
+--     vim.g.hulvdan_EnableAnimations()
+-- end
 
--- Bindings
-local opts = { remap = false, silent = true }
-vim.keymap.set("n", "<C-S-T>", ":BufferRestore<CR>", opts)
-vim.keymap.set("t", "<C-S-T>", ":BufferRestore<CR>", opts)
+-- -- Bindings
+-- local opts = { remap = false, silent = true }
+-- vim.keymap.set("n", "<C-S-T>", ":BufferRestore<CR>", opts)
+-- vim.keymap.set("t", "<C-S-T>", ":BufferRestore<CR>", opts)
 
--- vim.keymap.set("n", "<leader>v", select_previous_tab, opts)
--- vim.keymap.set("n", "<leader>m", select_next_tab, opts)
-vim.keymap.set("n", "<A-v>", select_previous_tab, opts)
-vim.keymap.set("n", "<A-m>", select_next_tab, opts)
-vim.keymap.set("n", "<A-S-v>", ":BufferMovePrevious<CR>", opts)
-vim.keymap.set("n", "<A-S-m>", ":BufferMoveNext<CR>", opts)
+-- -- vim.keymap.set("n", "<leader>v", select_previous_tab, opts)
+-- -- vim.keymap.set("n", "<leader>m", select_next_tab, opts)
+-- vim.keymap.set("n", "<A-v>", select_previous_tab, opts)
+-- vim.keymap.set("n", "<A-m>", select_next_tab, opts)
+-- vim.keymap.set("n", "<A-S-v>", ":BufferMovePrevious<CR>", opts)
+-- vim.keymap.set("n", "<A-S-m>", ":BufferMoveNext<CR>", opts)
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -249,66 +249,100 @@ return {
             })
         end
     },
+    -- {
+    --     "romgrk/barbar.nvim",
+    --     lazy = false,
+    --     init = function()
+    --         vim.g.barbar_auto_setup = false
+    --     end,
+    --     opts = {
+    --         animation = false,
+    --         auto_hide = 1,
+    --         maximum_padding = 1,
+    --         minimum_padding = 1,
+    --         semantic_letters = false,
+    --         highlight_inactive_file_icons = false,
+    --         tabpages = false,
+    --         icons = {
+    --             -- Configure the base icons on the bufferline.
+    --             -- Valid options to display the buffer index and -number are `true`, "superscript" and "subscript"
+    --             buffer_index = false,
+    --             buffer_number = false,
+    --             button = "",
+    --             -- Enables / disables diagnostic symbols
+    --             diagnostics = {
+    --                 [vim.diagnostic.severity.ERROR] = { enabled = false, icon = "E" },
+    --                 [vim.diagnostic.severity.WARN] = { enabled = false },
+    --                 [vim.diagnostic.severity.INFO] = { enabled = false },
+    --                 [vim.diagnostic.severity.HINT] = { enabled = false },
+    --             },
+    --             gitsigns = {
+    --                 added = { enabled = false, icon = "+" },
+    --                 changed = { enabled = false, icon = "~" },
+    --                 deleted = { enabled = false, icon = "-" },
+    --             },
+    --             filetype = {
+    --                 -- Sets the icon's highlight group.
+    --                 -- If false, will use nvim-web-devicons colors
+    --                 custom_colors = false,
+
+    --                 -- Requires `nvim-web-devicons` if `true`
+    --                 enabled = false,
+    --             },
+    --             separator = { left = "▎", right = "" },
+
+    --             -- If true, add an additional separator at the end of the buffer list
+    --             separator_at_end = false,
+
+    --             -- Configure the icons on the bufferline when modified or pinned.
+    --             -- Supports all the base icon options.
+    --             modified = { button = "" },
+    --             pinned = { button = "●", filename = true },
+
+    --             -- Use a preconfigured buffer appearance— can be "default", "powerline", or "slanted"
+    --             preset = "default",
+
+    --             -- Configure the icons on the bufferline based on the visibility of a buffer.
+    --             -- Supports all the base icon options, plus `modified` and `pinned`.
+    --             alternate = { filetype = { enabled = false } },
+    --             current = { buffer_index = false },
+    --             inactive = { button = "" },
+    --             visible = { modified = { buffer_number = false } },
+    --         },
+    --     },
+    -- },
     {
-        "romgrk/barbar.nvim",
+        "akinsho/bufferline.nvim",
+        version = "*",
         lazy = false,
-        init = function()
-            vim.g.barbar_auto_setup = false
+        -- dependencies = "nvim-tree/nvim-web-devicons",
+        config = function()
+            vim.opt.termguicolors = true
+
+            bufferline = require("bufferline")
+            bufferline.setup({
+                options = {
+                    style_preset = {
+                        bufferline.style_preset.no_italic,
+                        bufferline.style_preset.no_bold
+                    },
+                    separator_style = {"", ""},
+                    show_close_icon = false,
+                    show_buffer_close_icons = false,
+                    tab_size = 5,
+                },
+            })
+
+            local opts = { remap = false, silent = true }
+            -- vim.keymap.set("n", "<C-S-T>", ":BufferRestore<CR>", opts)
+            -- vim.keymap.set("t", "<C-S-T>", ":BufferRestore<CR>", opts)
+
+            -- vim.keymap.set("n", "<leader>v", select_previous_tab, opts)
+            -- vim.keymap.set("n", "<leader>m", select_next_tab, opts)
+            vim.keymap.set("n", "<A-v>", ":BufferLineCyclePrev<CR>", opts)
+            vim.keymap.set("n", "<A-m>", ":BufferLineCycleNext<CR>", opts)
+            vim.keymap.set("n", "<A-S-v>", ":BufferLineMovePrev<CR>", opts)
+            vim.keymap.set("n", "<A-S-m>", ":BufferLineMoveNext<CR>", opts)
         end,
-        opts = {
-            animation = false,
-            auto_hide = 1,
-            maximum_padding = 1,
-            minimum_padding = 1,
-            semantic_letters = false,
-            highlight_inactive_file_icons = false,
-            tabpages = false,
-            icons = {
-                -- Configure the base icons on the bufferline.
-                -- Valid options to display the buffer index and -number are `true`, "superscript" and "subscript"
-                buffer_index = false,
-                buffer_number = false,
-                button = "",
-                -- Enables / disables diagnostic symbols
-                diagnostics = {
-                    [vim.diagnostic.severity.ERROR] = { enabled = false, icon = "E" },
-                    [vim.diagnostic.severity.WARN] = { enabled = false },
-                    [vim.diagnostic.severity.INFO] = { enabled = false },
-                    [vim.diagnostic.severity.HINT] = { enabled = false },
-                },
-                gitsigns = {
-                    added = { enabled = false, icon = "+" },
-                    changed = { enabled = false, icon = "~" },
-                    deleted = { enabled = false, icon = "-" },
-                },
-                filetype = {
-                    -- Sets the icon's highlight group.
-                    -- If false, will use nvim-web-devicons colors
-                    custom_colors = false,
-
-                    -- Requires `nvim-web-devicons` if `true`
-                    enabled = false,
-                },
-                separator = { left = "▎", right = "" },
-
-                -- If true, add an additional separator at the end of the buffer list
-                separator_at_end = false,
-
-                -- Configure the icons on the bufferline when modified or pinned.
-                -- Supports all the base icon options.
-                modified = { button = "" },
-                pinned = { button = "●", filename = true },
-
-                -- Use a preconfigured buffer appearance— can be "default", "powerline", or "slanted"
-                preset = "default",
-
-                -- Configure the icons on the bufferline based on the visibility of a buffer.
-                -- Supports all the base icon options, plus `modified` and `pinned`.
-                alternate = { filetype = { enabled = false } },
-                current = { buffer_index = false },
-                inactive = { button = "" },
-                visible = { modified = { buffer_number = false } },
-            },
-        },
-    }
+    },
 }
