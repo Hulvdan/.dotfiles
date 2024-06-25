de2729f602830583adbfbe03214a9f782d78090d
Aleksei Chistov <aleksey.chistov@gmail.com>
2024-06-26 02:06:29 +0500

    shit

--- nvim/lua/hulvdan/remap.lua
+++ nvim/lua/hulvdan/remap.lua
@@ -22,15 +22,14 @@ end, opts)
 -- Binding Ctrl+\ Ctrl+N to ESC makes us able to leave the terminal mode
 vim.keymap.set("t", "<ESC>", [[<C-\><C-n>]], opts)
 
--- Binding Git functions
--- Ctrl-Shift-G + l - Opens lazygit
+-- Open lazygit
 vim.keymap.set("n", "gl", function()
     -- TODO: Make it OS agnostic
     -- TODO: Focus if it's already running (based on the window's title - there's a path displayed)
     vim.fn.system("start /MAX lazygit")
 end, opts)
 
--- Ctrl-Shift-G + l - Opens Github Desktop
+-- Open github desktop
 vim.keymap.set("n", "gd", function()
     vim.fn.system("github")
 end, opts)
@@ -39,32 +38,15 @@ end, opts)
 -- Also, in normal mode these jumps don't pollute the jumps list
 vim.keymap.set("n", "<C-J>", ":keepjumps norm! }<CR>", opts)
 vim.keymap.set("n", "<C-K>", ":keepjumps norm! {<CR>", opts)
-vim.keymap.set("n", "<C-S-J>", ":keepjumps norm! )<CR>", opts)
-vim.keymap.set("n", "<C-S-K>", ":keepjumps norm! (<CR>", opts)
 vim.keymap.set("v", "<C-J>", "}", opts)
 vim.keymap.set("v", "<C-K>", "{", opts)
-vim.keymap.set("v", "<C-S-J>", ")", opts)
-vim.keymap.set("v", "<C-S-K>", "(", opts)
-
--- vim.keymap.set("n", "<C-A-J", "<C-Down>", {silent = true, noremap = false})
--- vim.keymap.set("n", "<C-A-K", "<C-Up>", {silent = true, noremap = false})
--- vim.keymap.set("n", "<C-S-J", "<C-Down>")
--- vim.keymap.set("n", "<C-S-K", "<C-Up>")
--- vim.keymap.set("n", "<C-J", "<C-Down>")
--- vim.keymap.set("n", "<C-K", "<C-Up>")
-
-function WindowsCount()
-    local count = 0
-    for _, a in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
-        count = count + 1
-    end
-    return count
-end
+-- Broken because of windows' Ctrl Shift shiet in terminals
+-- vim.keymap.set("n", "<C-S-J>", ":keepjumps norm! )<CR>", opts)
+-- vim.keymap.set("n", "<C-S-K>", ":keepjumps norm! (<CR>", opts)
+-- vim.keymap.set("v", "<C-S-J>", ")", opts)
+-- vim.keymap.set("v", "<C-S-K>", "(", opts)
 
 vim.keymap.set({ "n", "t" }, "<A-q>", ":bdelete!<CR>", opts)
-
-vim.keymap.set("n", "<f2>", vim.diagnostic.goto_next, opts)
-vim.keymap.set("n", "<S-f2>", vim.diagnostic.goto_prev, opts)
 
 vim.keymap.set("n", [[<leader>\]], function()
     vim.fn.execute("vs")
@@ -85,17 +67,10 @@ vim.keymap.set("n", "<leader>n", OpenNotes, opts)
 vim.keymap.set("n", "<A-J>", "<C-Down>", { silent = true, remap = true })
 vim.keymap.set("n", "<A-K>", "<C-Up>", { silent = true, remap = true })
 
--- if vim.g.goneovim or vim.g.hulvdan_is_qt then
-vim.keymap.set("n", "<C-S-]>", ":cc<CR>:cn<CR>", opts)
-vim.keymap.set("n", "<C-S-[>", ":cc<CR>:cp<CR>", opts)
--- vim.keymap.set("n", "<f2>", ":cc<CR>:cn<CR>", opts)
--- vim.keymap.set("n", "<S-f2>", ":cc<CR>:cp<CR>", opts)
--- else
--- vim.keymap.set("n", "<C-}>", ":cc<CR>:cn<CR>", opts)
--- vim.keymap.set("n", "<C-{>", ":cc<CR>:cp<CR>", opts)
--- vim.keymap.set("n", "<C-S-}>", ":cc<CR>:cn<CR>", opts)
--- vim.keymap.set("n", "<C-S-{>", ":cc<CR>:cp<CR>", opts)
--- end
+-- vim.keymap.set("n", "<f2>", vim.diagnostic.goto_next, opts)
+-- vim.keymap.set("n", "<S-f2>", vim.diagnostic.goto_prev, opts)
+vim.keymap.set("n", "<f2>", ":cc<CR>:cn<CR>", opts)
+vim.keymap.set("n", "<S-f2>", ":cc<CR>:cp<CR>", opts)
 
 vim.keymap.set("n", "<M-[>", function()
     require("tree-climber").goto_parent()
@@ -123,7 +98,7 @@ function get_selected_text()
     return vim.api.nvim_buf_get_text(0, ls - 1, cs - 1, le - 1, ce, {})[1]
 end
 
--- Поиск
+-- Поиск в одном буфере
 vim.keymap.set("v", "<A-n>", function()
     local text = get_selected_text()
     vim.api.nvim_input("<ESC>/\\C" .. text .. "<CR>")
