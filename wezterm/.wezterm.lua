24542d1609bcf07e52e9047682cc3e88e3c0f9f9
Aleksei Chistov <aleksey.chistov@gmail.com>
2024-07-06 13:03:47 +0500

    wezterm: fixed font

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,15 +1,26 @@
 local wezterm = require("wezterm")
 local mux = wezterm.mux
 wezterm.on("gui-startup", function(cmd)
-	local tab, pane, window = mux.spawn_window(cmd or {})
-	window:gui_window():maximize()
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
 end)
 
 local config = wezterm.config_builder()
 
 config.color_scheme = "Gruvbox dark, hard (base16)"
-config.font =
-	wezterm.font("For use only at ilovetypography.com", { weight = "DemiBold", stretch = "Normal", style = "Normal" })
+config.font = wezterm.font({
+  family = "For use only at ilovetypography.com",
+  weight = "DemiBold",
+  style = "Normal",
+})
+config.font_rules = {
+  { intensity = "Half", italic = true, font = config.font },
+  { intensity = "Half", italic = false, font = config.font },
+  { intensity = "Bold", italic = true, font = config.font },
+  { intensity = "Bold", italic = false, font = config.font },
+  { intensity = "Normal", italic = true, font = config.font },
+  { intensity = "Normal", italic = false, font = config.font },
+}
 
 config.enable_csi_u_key_encoding = true
 config.allow_win32_input_mode = false
@@ -21,17 +32,17 @@ config.window_decorations = "NONE"
 config.font_size = 13
 
 config.keys = {
-	{ key = "=", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
-	{ key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
+  { key = "=", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
+  { key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
 }
 
 config.enable_tab_bar = false
 config.window_close_confirmation = "NeverPrompt"
 config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
 }
 
 return config
