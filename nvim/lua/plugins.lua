910eb452a80200388ab94900999603ed2b2695ea
Aleksei Chistov <aleksey.chistov@gmail.com>
2024-07-14 00:01:22 +0500

    nvim: zen mode

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -109,6 +109,40 @@ return {
     },
 
     {
+        "folke/zen-mode.nvim",
+        lazy = false,
+        opts = {
+            window = {
+                backdrop = 0.5, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+                -- height and width can be:
+                -- * an absolute number of cells when > 1
+                -- * a percentage of the width / height of the editor when <= 1
+                -- * a function that returns the width or the height
+                width = 90, -- width of the Zen window
+                -- height = 1, -- height of the Zen window
+                -- by default, no options are changed for the Zen window
+                -- uncomment any of the options below, or add other vim.wo options you want to apply
+                -- options = {
+                --     -- signcolumn = "no", -- disable signcolumn
+                --     -- number = false, -- disable number column
+                --     -- relativenumber = false, -- disable relative numbers
+                --     -- cursorline = false, -- disable cursorline
+                --     -- cursorcolumn = false, -- disable cursor column
+                --     -- foldcolumn = "0", -- disable fold column
+                --     -- list = false, -- disable whitespace characters
+                -- },
+            },
+        },
+        init = function()
+            local toggle_zen = function()
+                vim.fn.execute("ZenMode")
+            end
+            vim.keymap.set("n", "<A-S-z>", toggle_zen, { silent = true, remap = false })
+            vim.keymap.set("n", "Z", toggle_zen, { silent = true, remap = false })
+        end,
+    },
+
+    {
         "ojroques/nvim-bufdel",
         lazy = false,
         config = function()
