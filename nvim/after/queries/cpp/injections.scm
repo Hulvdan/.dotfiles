8ec51d22251e6286193e195fe0ddd71a6f02dafd
Aleksei Chistov <aleksey.chistov@gmail.com>
2024-07-14 00:01:53 +0500

    nvim: C++ with glsl injected

--- nvim/after/queries/cpp/injections.scm
+++ nvim/after/queries/cpp/injections.scm
@@ -1,13 +1,11 @@
-; IT DOES NOT WORK!!!
+;extends
 
 (raw_string_literal
-    delimiter: (raw_string_delimiter) @delimiter (#eq? @delimiter "VertexShader")
-    ; (raw_string_content) @vertex_shader_injected @sql
-    (raw_string_content) @glsl
-    (raw_string_delimiter))
+  delimiter: (raw_string_delimiter) @delimiter (#eq? @delimiter "VertexShader")
+  (raw_string_content) @injection.content (#set! injection.language "glsl")
+  (raw_string_delimiter))
 
 (raw_string_literal
-    delimiter: (raw_string_delimiter) @delimiter (#eq? @delimiter "FragmentShader")
-    ; (raw_string_content) @fragment_shader_injected @sql
-    (raw_string_content) @glsl
-    (raw_string_delimiter))
+  delimiter: (raw_string_delimiter) @delimiter (#eq? @delimiter "FragmentShader")
+  (raw_string_content) @injection.content (#set! injection.language "glsl")
+  (raw_string_delimiter))
