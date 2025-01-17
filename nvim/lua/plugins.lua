662240819b0d6def82c61c1f4f436296545a0fc5
Aleksei Chistov <aleksey.chistov@gmail.com>
2025-01-17 13:54:49 +0500

    nvim: big coq!

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -340,19 +340,15 @@ return {
                     winhighlight = true, -- Auto highlighting for focussed/unfocussed windows
                 },
             })
-
             local ignore_filetypes = { "neo-tree" }
             local ignore_buftypes = { "nofile", "prompt", "popup" }
-
             local augroup = vim.api.nvim_create_augroup("FocusDisable", { clear = true })
-
             vim.api.nvim_create_autocmd("WinEnter", {
                 group = augroup,
                 callback = function(_)
                     vim.w.focus_disable = vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
                 end,
             })
-
             vim.api.nvim_create_autocmd("FileType", {
                 group = augroup,
                 callback = function(_)
@@ -441,7 +437,7 @@ return {
             vim.g.coq_settings = {
                 auto_start = "shut-up",
                 limits = {
-                    tokenization_limit = 10000,
+                    tokenization_limit = 40000,
                 },
             }
         end,
